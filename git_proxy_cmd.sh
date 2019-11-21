7130f40f442b9c1b08d98492d7f69d6fa81888d7
Denis Romanchuk <den4ikkss@gmail.com>
2019-11-22 01:03:52 +0200

    sleep 1 sec before git cmd

--- git_proxy_cmd.sh
+++ git_proxy_cmd.sh
@@ -5,6 +5,7 @@ port=9999
 bind_address=localhost
 
 ssh workstation -D ${bind_address}:${port} -N &
+sleep 1
 echo git -c http.proxy=socks5://${bind_address}:${port} $*
 git -c http.proxy=socks5://${bind_address}:${port} $*
 kill $!
