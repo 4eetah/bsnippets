036c4b9e3eb8325388fb051200306a313c0e5a89
Denis Romanchuk <den4ikkss@gmail.com>
2019-12-12 00:20:36 +0200

    git_proxy_cmd handle ctr-c

--- git_proxy_cmd.sh
+++ git_proxy_cmd.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 pushd $PWD
 
+trap SIGINT_handler INT
+
+function SIGINT_handler() {
+    kill $tunnelPID    
+}
+
 port=9999
 bind_address=localhost
 
 ssh workstation -D ${bind_address}:${port} -N &
+tunnelPID=$!
 sleep 1
 echo git -c http.proxy=socks5://${bind_address}:${port} $*
-git -c http.proxy=socks5://${bind_address}:${port} $*
-kill $!
+git -c http.proxy=socks5://${bind_address}:${port} $@
+kill $tunnelPID
 popd
